050d685237469f8d936298a40e1da9ce06b36cf4
Patrique Porto <patrikrock@hotmail.com>
2023-10-26 12:10:41 -0300

    add header component, mobile component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,53 @@ import Link from "next/link"
 import { motion } from "framer-motion"
 import { fadeIn } from "../../variants"
 
+// Components
+import NavMobile from "./NavMobile"
+import Nav from "./Nav"
+
 const Header = () => {
+
+  const [active, setActive] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Detect scroll
+      setActive(window.scrollY > 100)
+    }
+     // Add event listener
+    window.addEventListener('scroll', handleScroll)
+
+    // Clear event listener
+    return () => { 
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <div>Header</div>
+    <header className={`fixed z-50 w-full ${active ? 'bg-[#030315] py-6' : 'bg-transparent py-8'}`}>
+       <div className="container mx-auto flex flex-col xl:flex-row items-center justify-between">
+           {/* Logo */}
+           <Link
+               href="#"
+               className="relative flex w-[226px] h-[37.64px] transition-all mb-4 xl:mb-0"
+           >
+               <Image
+                   src="/assets/header/logo.svg"
+                   fill
+                   className="object-contain"
+                   alt="logo"
+               />
+           </Link>
+           {/* Nav */}
+           <Nav />
+           {/* Nav mobile */}
+           <NavMobile />
+           {/* Menu btn */}
+           <div>menu btn</div>
+           {/* Social */}
+           <div>social icons</div>
+       </div>
+    </header>
   )
 }
 
