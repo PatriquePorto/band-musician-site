57e73bec0f714a67978c6ad548478e154fe11c5e
Patrique Porto <patrikrock@hotmail.com>
2023-10-31 17:17:46 -0300

    add album, music and playter component

--- src/components/albums/AlbumSlider.jsx
+++ src/components/albums/AlbumSlider.jsx
@@ -17,8 +17,93 @@ import "swiper/css/pagination"
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 const AlbumSlider = () => {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null)
+
+  const { data, error } = useSWR('http://localhost:4000/albums', fetcher)
+ 
+  if (error) return 'Failed to fetch data'
+  if (!data) return 'Loading...'
+
   return (
-    <div>AlbumSlider</div>
+    <>
+       {/* Top Slider*/}
+       <Swiper
+         effect={'coverflow'}
+         speed={1000}
+         spaceBetween={80}
+         allowTouchMover={false}
+         thumbs={{ swiper: thumbsSwiper }}  
+         modules={[EffectCoverflow, FreeMode, Navigation, Thumbs]}
+         coverflowEffect={{
+           rotate: 50,
+           stretch: 0,
+           depth: 100,
+           modifier: 1,
+           slideShadows: true,
+         }}
+         className="album-slider">
+          {data.map((album) => {
+            return (
+            <SwiperSlide key={album.id} className="mb-12">
+               <div className="w-full bg-secondary/80 rounded-[10px] flex flex-col xl:flex-row items-center
+                  p-6 xl:p-12 gap-x-12 ">
+                  {/* Image */}
+                  <div className="hidden xl:flex w-[300px] h-[300px] xl:w-[500px] xl:h-[500px] relative
+                                 cursor-pointer rounded-[10px] overflow-hidden ">
+                          <Image 
+                          src={album.img} 
+                          fill
+                          priority
+                          alt=""
+                          className="object-contain"
+                      />
+                  </div>
+                  {/* Track Container */}
+                  <div className="flex flex-1 w-full h-[500px]">
+                     {/* Track List */}
+                    <div className="flex-1 flex flex-col xl:px-12">                 
+                     {album.tracks?.map((track, index) => {
+                        return (
+                        <div className="flex flex-1 w-full h-[500px]"
+                          key={index}>
+                          {/* Track Name */}
+                           <div className="flex flex-1 items-center gap-x-2 capitalize font-semibold xl:font-extrabold"> 
+                              <div className="text-accent text-sm xl:text-lg">0{index + 1}.</div>
+                              <div className="text-sm xl:text-base">{track.name}</div>
+                            </div>
+                           {/* Player */}
+                          <div>
+                            <AudioPlayer
+                              src={track.src}
+                              loop
+                              preload="none"
+                              color="#7F1CFC"
+                              sliderColor="#7F1CFC"
+                              style={{
+                                background: 'transparent',
+                                maxWidth: '100%',
+                                paddingLeft: '0px',
+                                paddingRight: '0px',
+                              }}
+                              volume={50}
+                              volumePlacement="bottom"
+                              className="album-player"
+                            />
+                          </div>
+                        </div>
+                       )
+                     })}
+                  </div>
+                 </div>
+               </div>
+            </SwiperSlide>     
+            )      
+          })}    
+       </Swiper>    
+       {/* Thumb Slider */}
+       <Swiper>Thumb slider</Swiper>
+    </>
+    
   )
 }
 
